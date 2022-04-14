9f6db5c6e6aa4c24be73ae175c86185ceaa544a3
Gajabheemkar Akanksha <96841062+Akankshapc@users.noreply.github.com>
2022-04-14 12:49:54 +0530

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,16 @@
+#task5
+import json
+from bs4 import BeautifulSoup
+from task1 import scraped_movie_details
+from task4 import first_movie
+movies = scraped_movie_details()
+def get_movie_details():  
+    movie_list = []
+    for i in movies:
+        url1=i["URL"]
+        a=first_movie(url1)
+        movie_list.append(a)
+    with open("task5.json","w+") as file5:
+        json.dump(movie_list,file5,indent=4)
+    return movie_list
+get_movie_details()
